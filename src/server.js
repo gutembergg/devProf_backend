4e0060645714e10666c177adb3db7e0dd8f5450e
Gutembergue Mascarenhas <gmascarenhas3001@gmail.com>
2020-08-20 23:02:20 +0200

    add cors function

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const app = express()
 // Middlewares ////////////////////////////////////////////////////////////////////////
 
 app.use(express.json())
-app.use(cors({ origin: 'https://devprof-backend.herokuapp.com' }))
+app.use(cors('*'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
