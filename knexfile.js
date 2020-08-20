95fb9d240c059e7107801e82c56c54c1c5ca5cd2
Gutembergue Mascarenhas <gmascarenhas3001@gmail.com>
2020-08-20 20:57:38 +0200

    add env variable in knexfile

--- knexfile.js
+++ knexfile.js
@@ -36,17 +36,14 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/database/migrations`
     }
   }
 }
